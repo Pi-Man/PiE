3531060247027bb61c0b5f49a0678dbd1261d53c
Pi Man <>
2021-05-20 15:27:54 -0700

    updated from a simple square to mandelbrot renderer

--- TemplateProject/TemplateProject/main.cpp
+++ TemplateProject/TemplateProject/main.cpp
@@ -1,16 +1,41 @@
 #include <PiE.h>
 
+Camera5DoF *camera;
+float zoom = 1;
+
+void cameraControlls(PiE::EngineContext &ctx, SDL_Event event) {
+	switch (event.key.keysym.sym) {
+	case SDLK_w:
+		camera->move(0.0f, zoom / 10.0f, 0.0f);
+		break;
+	case SDLK_s:
+		camera->move(0.0f, -zoom / 10.0f, 0.0f);
+		break;
+	case SDLK_d:
+		camera->move(zoom / 10.0f, 0.0f, 0.0f);
+		break;
+	case SDLK_a:
+		camera->move(-zoom / 10.0f, 0.0f, 0.0f);
+		break;
+	}
+}
+
+void zoomCamera(PiE::EngineContext &ctx, SDL_Event event) {
+	zoom *= pow(1.125f, event.wheel.y);
+	camera->FOV = 1/zoom;
+}
+
 int main(int argc, char** args) {
 	//-------create context and initialize engine---------
 	PiE::EngineContext ctx;
 
 	PiE::initEngine(ctx);
 
-	SDL_SetWindowTitle(ctx.mainWindow, "Hello Square");
+	SDL_SetWindowTitle(ctx.mainWindow, "Mandelbrot Set");
 	//----------set up main camera and shader-------------
-	Camera5DoF camera(ctx.mainWindow);
+	camera = new Camera5DoF(ctx.mainWindow);
 
-	ctx.mainCamera = &camera;
+	ctx.mainCamera = camera;
 
 	Shader shader;
 
@@ -19,20 +44,23 @@ int main(int argc, char** args) {
 	ctx.mainShader = shader.ID;
 	//------------add in renderable objects---------------
 	RenderObject RO;
-	RO.VAO = VertexArrayObject(VAO_NORMALS); // using normals for color because color isn't implemented yet
-	RO.VAO.addQuadWithNormals({
-		-0.5, -0.5, 1.0,
+	RO.VAO = VertexArrayObject();
+	RO.VAO.addQuadWithNormals({ // simple rect from 2,2 to -2,-2 to display mandelbrot results
+		-4, -4, 1.0,
 			1.0, 0.0, 0.0,
-		0.5, -0.5, 1.0,
+		4, -4, 1.0,
 			0.0, 1.0, 0.0,
-		0.5, 0.5, 1.0,
+		4, 4, 1.0,
 			0.0, 0.0, 1.0,
-		-0.5, 0.5, 1.0,
+		-4, 4, 1.0,
 			1.0, 1.0, 1.0 });
 	
 	PiE::addMesh(ctx, RO);
+	//--------------add in event listeners----------------
+	ctx.events.insert({ SDL_EventType::SDL_KEYDOWN, cameraControlls });
+	ctx.events.insert({ SDL_EventType::SDL_MOUSEWHEEL, zoomCamera });
 	//-------------------run main loop--------------------
-	camera.move(0, 0, -1);
+	camera->move(0, 0, -1);
 	PiE::startMainGameLoop(ctx, true);
 
 	return 0;
